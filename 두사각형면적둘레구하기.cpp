32398a36486065363c83866f1ab8fa0697acd821
jodaun <awhekdns@kookmin.ac.kr>
2023-04-12 20:50:44 +0900

    Update 두사각형면적둘레구하기.cpp

---
+++ 두사각형면적둘레구하기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int x1,y1,x2,y2;
+        int x3,y3,x4,y4;
+
+        cin >> x1 >> y1 >> x2 >> y2;
+        cin >> x3 >> y3 >> x4 >> y4;
+
+        int getOver
+        
+       
+
+
+    }
+}
